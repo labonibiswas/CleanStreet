9aade32c9580ebfbf579102b09d76d5d9f4d4d4c
Rasha-Syed <rashanishath@gmail.com>
2026-02-18 12:55:55 +0530

    Updated login and register routes

--- backend/server.js
+++ backend/server.js
@@ -4,24 +4,19 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 
 dotenv.config();
-
-const app = express();
-
-
 connectDB();
 
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", require("./routes/authRoutes"));
 
 app.get("/", (req, res) => {
-  res.send("CleanStreet Backend Running");
+  res.send("CleanStreet API Running...");
 });
-
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
