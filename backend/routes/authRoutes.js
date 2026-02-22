85a3ae7432a8180c8a2fa4eef80a2a6bfb18c406
Rasha-Syed <rashanishath@gmail.com>
2026-02-22 19:56:28 +0530

    Add change password and update profile routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { registerUser, loginUser } = require("../controllers/authController");
+
+const { registerUser, loginUser, changePassword, updateProfile } = require("../controllers/authController");
+
+const protect = require("../middleware/authMiddleware"); 
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.put("/change-password", protect, changePassword);
+router.put("/update-profile", protect, updateProfile);
 
 module.exports = router;
